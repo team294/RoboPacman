87ddb8bce67df792050631a02d95585cda9756b5
BCR <bcr@bcr.org>
2019-10-27 15:51:25 -0700

    Robo pac man draft 2

--- pacman/src/main/java/pacman/commands/AutoGroup.java
+++ pacman/src/main/java/pacman/commands/AutoGroup.java
@@ -7,9 +7,22 @@ public class AutoGroup extends CommandGroupBase{
 	public AutoGroup() {
 		addSequential(new DriveStraight(6));
 		addSequential(new Turn(270));  
-		addSequential(new DriveStraight(15));
+		addSequential(new DriveStraight(16));
+		addSequential(new Turn(180));
+		addSequential(new DriveStraight(22));
+		addSequential(new Turn(90));
+		addSequential(new DriveStraight(31));
 		addSequential(new Turn(0));
-		addSequential(new DriveStraight(6));
+		addSequential(new DriveStraight(34));
+		addSequential(new Turn(270));
+		addSequential(new DriveStraight(42));
+		addSequential(new Turn(90));
+		addSequential(new DriveStraight(46));
+		addSequential(new Turn(0));
+		addSequential(new DriveStraight(48));
+		addSequential(new Turn(180));
+		addSequential(new DriveStraight(55));
+		
 	} 
 	
 }
