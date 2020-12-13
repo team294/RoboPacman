a8f6c8256c8c68fc51511010eca9dd688517bcf5
Paul Krinsky <pkrinsky@gmail.com>
2020-12-12 22:34:30 -0800

    Updated ghost movement to be horiz or vert

--- pacman/src/main/java/pacman/graphics/Ghost.java
+++ pacman/src/main/java/pacman/graphics/Ghost.java
@@ -6,6 +6,7 @@ public class Ghost {
 	protected int x;
 	protected int y;
 	protected int direction = 0;
+	protected int moveY = 0;
 	protected int duration = 0;
 	
 	public Ghost(int x, int y) {
@@ -13,35 +14,44 @@ public class Ghost {
 		this.x = x;
 		this.y = y;
 	}
+
 	public int getX() {
 		return x;
 	}
+
 	public void setX(int x) {
-		this.x = x;
+		if (x >= 1 && x < PacmanGraphics.COLS) {
+			this.x = x;
+		}
 	}
+
 	public int getY() {
 		return y;
 	}
+
 	public void setY(int y) {
-		this.y = y;
+		if (y >= 1 && y < PacmanGraphics.ROWS) {
+			this.y = y;
+		}
 	}
 
 	public void move(int pacmanX, int pacmanY) {
-		int next = 0;
-
 		if (duration == 0) {
 			duration = ThreadLocalRandom.current().nextInt(1, 6);
 			direction = ThreadLocalRandom.current().nextInt(-1, 2);
-			System.out.printf("ghost duration:%d direction:%d \n",duration,direction);
+			moveY = ThreadLocalRandom.current().nextInt(0, 2);
+			//System.out.printf("ghost duration:%d direction x:%d y:%d %n",duration,direction, directionY);
 		} else {
 			duration--;
 		}
 
-		// ghosts only move horizontally
-		next = getX()+direction;
-
-		// don't limit ghosts position so they can move off the screen and then come
-		setX(next);
+		// check to see if we should move on Y axis or X axis
+		if (moveY > 0) {
+			setY(getY()+direction);
+		} else {
+			setX(getX()+direction);
+		}
+		
 	}
 
 }
