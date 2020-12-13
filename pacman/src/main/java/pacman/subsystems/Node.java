530ea259bf13849e347ca51026ced4ffdd3a2781
griffing52 <emailgriffinnow@gmail.com>
2020-12-13 14:14:11 -0800

    Revert "Updated from save"
    This reverts commit d19d470ba686428d4acd6020733549f6a1b82638.

--- pacman/src/main/java/pacman/subsystems/Node.java
+++
@@ -1,9 +0,0 @@
-package pacman.subsystems;
-
-public class Node {
-    public int f;
-    public boolean OPENED = false;
-    public Node(Coord point, Coord goalPos, int h) {
-        this.f = (int) point.distance(goalPos)*10 + h;
-    }
-}
